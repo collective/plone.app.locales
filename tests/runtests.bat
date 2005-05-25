47aa08dd16938ea8c4be8fdc568d8641dc1792e0
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-05-25 08:07:03 +0000

    minor test cleanup and correcting some po's
    git-svn-id: http://svn.plone.org/svn/collective/PloneTranslations/trunk@8692 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -1,6 +1,3 @@
-@rem NB: Due to some sloppy coding and use of GNU's msgcmp,
-@rem     this won't test if all msgid's are present on Windows.
-
 @rem full path to the python interpretor
 @set PYTHON="d:\zope2\python\python.exe"
 
@@ -9,8 +6,5 @@
 
 @rem path to your instance. Don't set it if you aren't having  instance
 @set INSTANCE_HOME="D:\zope2\Data"
-
-@rem Make sure the gettext tools are on your PATH
-@set PATH=%PATH%;D:\zope2\gettext\bin
 
 %PYTHON% runalltests.py
