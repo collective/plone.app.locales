15ae3d017d486539797d6911b8654f27897cd02f
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-12-02 16:45:09 +0000

    Fix most of the issues in http://plone.org/collector/4840
    git-svn-id: http://svn.plone.org/svn/collective/PloneTranslations/trunk@14515 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- i18n/utils/relocate.py
+++ i18n/utils/relocate.py
@@ -31,56 +31,25 @@ def main():
         print 'No po-files were found for Plone.'
         sys.exit(2)
 
-    poFilesATCT = getPoFiles('atcontenttypes')
-    if not poFilesATCT:
-        print 'No po-files were found for ATContentTypes.'
+    poFilesAT = getPoFiles('archetypes')
+    if not poFilesAT:
+        print 'No po-files were found for Archetypes.'
         sys.exit(3)
 
     # format: 'old-domain' : ['msgid1', 'msgid2', ...]
-    # currently only relocating between atcontenttypes and plone and vica versa is possibly
-    relocated = {'atcontenttypes' :
-                  ['A boolean criterion', 'A date criteria',
-                   'A date range criterion', 'A list criterion', 'A path criterion',
-                   'A portal_types criterion', 'A reference criterion', 'A selection criterion',
-                   'A simple int criterion', 'A simple string criterion', 'help_limit_number',
-                   'A criterion that searches for the currently logged in user\'s id',
-                   'help_event_attendees', 'help_item_count',
-                   'help_string_criteria_value', 'label_event_attendees',
-                   'label_item_count', 'label_limit_number',
-                   'label_string_criteria_value',
-                   'An item\'s title transformed for sorting', 'An item\'s type (e.g. Event)',
-                   'An item\'s workflow state (e.g.published)', 'Boolean (True/False)',
-                   'Creation Date', 'CreationDate', 'Creator', 'Date range', 'Description',
-                   'Effective Date', 'EffectiveDate', 'End Date', 'Expiration Date',
-                   'ExpirationDate', 'Find items related to the selected items',
-                   'Integer value or range', 'Item Type', 'List of values', 'Location',
-                   'Location in portal', 'Modification Date', 'ModificationDate',
-                   'Related To', 'Relative date', 'Restrict to current user',
-                   'Search Text', 'SearchableText', 'Select content types',
-                   'Select referenced content', 'Select values from list', 'Short Name',
-                   'Size', 'Start Date', 'State', 'Subject', 'Text', 'start',
-                   'Text search of an item\'s contents', 'Text search of an item\'s title',
-                   'The end date and time of an event', 'The location an item in the portal (path)',
-                   'The short name of an item (used in the url)', 'The size of an item',
-                   'The start date and time of an event', 'The time and date an item becomes publicly available',
-                   'The time and date an item is no longer publically available',
-                   'The time and date an item is no longer publicly available',
-                   'The time and date an item was created', 'The time and date an item was last modified',
-                   'Title', 'Type', 'created', 'effective', 'end', 'expires', 'getId', 'getObjSize',
-                   'getRawRelatedItems', 'location', 'modified', 'path', 'review_state', 'sortable_title'
-                  ],
-                 'plone' :
-                  ['help_int_criteria_direction', 'help_int_criteria_value2',
-                   'label_int_criteria_direction', 'label_int_criteria_value2'
+    # currently only relocating between archetypes and plone and vica versa is possibly
+    relocated = {'plone' :
+                  ['description_edit_properties', 'heading_edit_item',
+                   'label_existing_keywords', 'label_new_keywords'
                   ]
                 }
 
     # make sure we only try on languages for which both po files exist
     ploneLanguages = [getLanguage('plone', p) for p in poFilesPlone]
-    atctLanguages = [getLanguage('atcontenttypes', p) for p in poFilesATCT]
+    atLanguages = [getLanguage('archetypes', p) for p in poFilesAT]
 
-    languages = [l for l in ploneLanguages if l in atctLanguages and l != 'en']
-    changes = {'plone' : False, 'atcontenttypes' : False}
+    languages = [l for l in ploneLanguages if l in atLanguages and l != 'en']
+    changes = {'plone' : False, 'archetypes' : False}
 
     for lang in languages:
         po_ctl = {}
@@ -89,17 +58,17 @@ def main():
         except IOError, e:
             print >> sys.stderr, 'I/O Error: %s' % e
         try:
-            po_ctl['atcontenttypes'] = catalog.MessageCatalog(filename='atcontenttypes-%s.po' % lang)
+            po_ctl['archetypes'] = catalog.MessageCatalog(filename='archetypes-%s.po' % lang)
         except IOError, e:
             print >> sys.stderr, 'I/O Error: %s' % e
 
-        changes = {'plone' : False, 'atcontenttypes' : False}
+        changes = {'plone' : False, 'archetypes' : False}
 
-        relocate_domain = {'plone' : 'atcontenttypes', 'atcontenttypes' : 'plone'}
+        relocate_domain = {'plone' : 'archetypes', 'archetypes' : 'plone'}
 
         msgids = {}
         msgids['plone'] = po_ctl['plone'].keys()
-        msgids['atcontenttypes'] = po_ctl['atcontenttypes'].keys()
+        msgids['archetypes'] = po_ctl['archetypes'].keys()
 
         for old_domain in relocated:
             relocate_msgids = relocated.get(old_domain)
