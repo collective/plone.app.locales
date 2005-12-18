dd0d3fd986f8037f4e5dd4708f7eaba5da75b1b8
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-12-18 21:56:04 +0000

    Updated test_i18n.py to i18ntestcase 1.2 syntax which supports the standard Zope3 locales folder layout.
    git-svn-id: http://svn.plone.org/svn/collective/PloneTranslations/trunk@15073 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -16,10 +16,11 @@ head, tail = os.path.split(PACKAGE_HOME)
 if tail == 'tests':
     PACKAGE_HOME = os.path.join(PACKAGE_HOME, '..')
 
-i18ndir = os.path.join(PACKAGE_HOME, 'i18n')
+i18ndir = os.path.normpath(PACKAGE_HOME)
 
 tests=[]
 products=[]
+pots={}
 pot_catalogs={}
 pot_lens={}
 
@@ -30,13 +31,14 @@ for potFile in getPotFiles(path=i18ndir):
     if product not in pot_catalogs:
         cat = catalog.MessageCatalog(filename=potFile)
         cat_len = len(cat)
+        pots.update({product: potFile})
         pot_catalogs.update({product: cat})
         pot_lens.update({product: cat_len})
 
 for product in products:
     class TestOnePOT(PotTestCase.PotTestCase):
-        pot = product
-        path = i18ndir
+        product = product
+        pot = pots[product]
     tests.append(TestOnePOT)
 
     for poFile in getPoFiles(path=i18ndir, product=product):
