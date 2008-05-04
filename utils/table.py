eb5cbdb63af80708cd12a1d55e6a49e95aff1d63
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-04 16:34:29 +0000

    New table script using a new i18ndude feature...
    git-svn-id: http://svn.plone.org/svn/collective/PloneTranslations/trunk@64329 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ utils/table.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+   Usage: table.py
+"""
+
+import os, sys
+from utils import getProduct, getPotFiles, getLongProductName
+from utils import PRODUCTS
+
+__I18NDUDE = os.environ.get('I18NDUDE', 'i18ndude')
+
+def main():
+    option = 'all'
+    if len(sys.argv) > 1:
+        option = sys.argv[1]
+
+    os.chdir('..')
+    os.chdir('i18n')
+
+    products = None
+    if option == 'all':
+        products = [getProduct(p) for p in getPotFiles()]
+    elif option in PRODUCTS.keys():
+        products = (getLongProductName(option), )
+
+    if products:
+        os.system(__I18NDUDE + (' list -t --products %s') % (' '.join(products)))
+
+if __name__ == '__main__':
+    main()
