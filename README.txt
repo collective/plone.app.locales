916de3664024dc0dd9e9c8d4ad73ad6514be993e
Vincent Fretin <vincent.fretin@gmail.com>
2012-09-03 13:20:18 +0200

    Added package structure

---
+++ README.txt
@@ -0,0 +1,9 @@
+Introduction
+============
+
+This package contains the translation files for Plone Core and the following
+non core add-ons:
+
+- LinguaPlone
+- plone.app.caching
+- plone.app.ldap
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '4.0.16dev'
+
+setup(name='plone.app.locales',
+      version=version,
+      description="Translation files for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='plone i18n locale translation',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.locales',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      )
