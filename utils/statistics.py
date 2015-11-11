ee47be4c860133209cb05ecff62473c6847d8601
Vincent Fretin <vincent.fretin@gmail.com>
2015-11-11 17:24:55 +0100

    fix statistics.py script

--- utils/statistics.py
+++ utils/statistics.py
@@ -1,7 +1,7 @@
 """
 Execute the following commands:
 cd ..
-LANG=en; for po in `find . -name "*.po" |grep -v "kupupox"` ; do echo -n "$po:";msgfmt -o /dev/null --statistics $po 2>&1; done > /tmp/results.txt
+LANG=en; for po in `find . -name "*.po"` ; do echo -n "$po:";msgfmt -o /dev/null --statistics $po 2>&1 |tail -n1; done > /tmp/results.txt
 cd utils
 python statistics.py /tmp/results.txt
 or if you want only your language to be displayed, here french:
@@ -18,32 +18,24 @@ import sys
 
 DOMAINS = ("atcontenttypes",
            "atreferencebrowserwidget",
-           "linguaplone",
-           "plone",
-           "passwordresettool",
-           "cmfplacefulworkflow",
            "cmfeditions",
-           "kupu",
-           "kupuconfig",
-#           "kupupox",
-           "plonelocales",
+           "cmfplacefulworkflow",
+           "passwordresettool",
+           "plone",
            "plonefrontpage",
+           "plonelocales",
+           "widgets",
            )
 
 result_file = open(sys.argv[1])
 
 stats = {}
 for line in result_file.xreadlines():
+    print line
     path, res = line.split(':')
-    if path.startswith('./i18n/'):
-        po, ext = os.path.splitext(os.path.basename(path))
-        domain, language = po.split('-', 1)
-    else:
-        language = path.split('/')[-3].replace('_', '-')
-        domain = os.path.splitext(os.path.basename(path))[0]
+    language = path.split('/')[-3].replace('_', '-')
+    domain = os.path.splitext(os.path.basename(path))[0]
     language = language.lower()
-    #print domain, language
-
     translated, fuzzy, untranslated = 0, 0, 0
     for part in res.split(','):
         n, mtype, dontcare = part.split()
@@ -73,25 +65,29 @@ if len(sys.argv) > 2:
 for language, domains in stats.items():
     if lg is not None and lg != language:
         continue
+
     total = [0,0,0]
     details = ''
     for domain in DOMAINS:
         if domain not in domains:
             details += "\t/!\\ %s file missing\n" % domain
             continue
+
         values = domains[domain]
         total[0] += values[0]
         total[1] += values[1]
         total[2] += values[2]
         sum_values = sum(values)
         details += "\t%s: %d/%d/%d = %d\n" % (domain, values[0], values[1], values[2], sum_values)
-        if sum_values != sum(stats['fr'][domain]):
+        if sum_values != sum(stats['fr'][domain]):  # French language has all po files, so this is our reference
             details += "\t/!\\ %s NOT SYNCHRONISED\n" % domain
+
     sum_total = sum(total)
     if sum_total == 0:
         percent = 0.0
     else:
         percent = round(float(total[0]) / float(sum_total) * 100.0, 2)
+
     print "%s\t: %d (%.2f%%) / %d / %d = %d" % (language,
                 total[0],
                 percent,
