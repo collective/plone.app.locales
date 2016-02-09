1b5e1221eb1e751d87c1de4543b70b1ddfe29b9d
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:09:22 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,29 +2,32 @@ from setuptools import setup, find_packages
 
 version = '5.0.8.dev0'
 
-setup(name='plone.app.locales',
-      version=version,
-      description="Translation files for Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='plone i18n locale translation',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://github.com/collective/plone.app.locales',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      )
+setup(
+    name='plone.app.locales',
+    version=version,
+    description="Translation files for Plone",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone i18n locale translation',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/collective/plone.app.locales',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    )
