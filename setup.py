81d4dc8fb5ad42204f6cd573e419017e1accaef1
Maurits van Rees <maurits@vanrees.org>
2019-08-29 23:02:29 +0200

    Added development status mature to setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
     classifiers=[
+        'Development Status :: 6 - Mature',
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: 5.1',
